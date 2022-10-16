444dfecd723d6b45a65b973b2e60bb1d0a9466ac
Jack Madden <jackmadden13@hotmail.co.uk>
2022-10-16 23:23:34 +0100

    First commit

---
+++ list.py
@@ -0,0 +1,12 @@
+def find_all_hobbyists(hobbies):
+    return key1
+if __name__ == "__main__":
+    hobbies = {
+        "Steve": ['Fashion', 'Piano', 'Reading'],
+        "Patty": ['Drama', 'Magic', 'Pets'],
+        "Chad": ['Puzzles', 'Pets', 'Yoga']
+    }
+for key, value in hobbies.items():
+    if value == value:
+        key1 = key
+print(find_all_hobbyists('Yoga', hobbies))
